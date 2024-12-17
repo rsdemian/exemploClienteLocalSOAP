ffa03816420de7f7e11d6d967e778009e5f9a197
Danilo Dias <danilo.drums25@gmail.com>
2024-12-16 22:52:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const soap = require('soap')
+var url = 'http://localhost:8001/wscalc1?wsdl';
+
+soap.createClient(url, function(err, client) {
+    if (err) throw err;
+
+    client.multiplicar({a: 4,b: 3},function(err,res){
+        if (err) throw err;
+        console.log(res);
+    });
+    
+    client.somar({a: 4,b: 3},function(err,res){
+        if (err) throw err;
+        console.log(res);
+    });
+});
